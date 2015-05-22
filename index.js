69bd80a52b43b403fbf905e16a1f6a27f8b05c7b
lucianlature <lucian.lature@gmail.com>
2015-05-22 15:31:53 +0100

    Initial commit.

---
+++ index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+// Import non-js files like this, with the extension and an exclamation point:
+import App from './App.jsx!'
+
+(() => {
+  React.render(
+    <App name="Lucian" />,
+    document.getElementById('react-app')
+  )
+})()
